85734648c4aefae829bafce8c3093242bd488124
dalvorsn <dalvo_rsn@hotmail.com>
2020-02-20 20:27:30 -0300

    Adição de models  - Etiqueta  - Postagem  - Revisao  - Classificacao  - Comentario

--- Blog/Models/Blog/Postagem/PostagemEntity.cs
+++ Blog/Models/Blog/Postagem/PostagemEntity.cs
@@ -1,5 +1,8 @@
 ﻿using Blog.Models.Blog.Autor;
 using Blog.Models.Blog.Categoria;
+using Blog.Models.Blog.Etiqueta;
+using Blog.Models.Blog.Postagem.Revisao;
+using System.Collections.Generic;
 
 namespace Blog.Models.Blog.Postagem
 {
@@ -8,5 +11,7 @@ namespace Blog.Models.Blog.Postagem
         public string Titulo;
         public AutorEntity Autor;
         public CategoriaEntity Categoria;
+        public List<EtiquetaEntity> Etiquetas;
+        public List<RevisaoEntity> Revisoes;
     }
 }
